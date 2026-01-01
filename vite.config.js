175fbbef3c85e852c5f195a83774e30a3b3c07c9
azizbargoth <azizbargoth@gmail.com>
2026-01-01 09:43:30 +0300

    Update Vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ExchangeOffice/',
   plugins: [react({
       babel: {
         plugins: ['babel-plugin-react-compiler'],
